2e5228e8ddf2b7dca8d598daae755b000230dae1
gasprod <gasprod13@gmail.com>
2017-01-07 20:38:51 +0100

    first commit

---
+++ GasSdFatInitSdType.ino
@@ -0,0 +1,17 @@
+#include <FreeStack.h>
+#include <MinimumSerial.h>
+#include <SdFat.h>
+#include <SdFatConfig.h>
+#include <SdFatUtil.h>
+#include <SystemInclude.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
